84dc180f1fa8526eccbf2041415f98cdd0f6029f
guitarYD <krauser085@gmail.com>
2019-02-19 22:06:32 +0900

    initial commit

---
+++ Core/Router.php
@@ -0,0 +1,13 @@
+<?php
+class Router {
+
+  protected $routes = [];
+
+  public function add($route, $params) {
+    $this->routes[$route] = $params;
+  }
+
+  public function getRoutes() {
+    return $this->routes;
+  }
+}
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require('../Core/Router.php');
+
+$router = new Router();
+
+$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('/posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
